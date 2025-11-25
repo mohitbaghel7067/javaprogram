1178240005a6945da0c1520f65480100bcbe3ea6
mohitbaghel7067 <mohitmb6261@gmail.com>
2025-11-25 15:18:56 +0530

    collection

---
+++ Let_10/collection.java
@@ -0,0 +1,15 @@
+//package Let_10;
+import java.util.ArrayList;
+public class collection {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(5);
+        list.add(10);
+        list.add(30);
+        list.add(20);
+        list.add(35);
+        System.out.println(list);
+
+    }
+    
+}
