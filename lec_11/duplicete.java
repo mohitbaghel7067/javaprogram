d15fce6a0352fbad436ff2fb4d4ac50b2fc41782
mohitbaghel7067 <mohitmb6261@gmail.com>
2025-11-28 13:59:07 +0530

    duplicete

---
+++ lec_11/duplicete.java
@@ -0,0 +1,14 @@
+package lec_11;
+import java.util.HashSet;
+class duplicate {
+    
+        public static void main(String[] args) {
+            HashSet<Integer>set=new HashSet<>();
+            set.add(1);
+            set.add(2);
+            set.add(1);
+            set.add(4);
+            System.out.println(set);
+        }
+    }
+    
