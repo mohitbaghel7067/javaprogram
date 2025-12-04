f67a4558a5d4acd6a00e3977918ec48c7646d091
mohitbaghel7067 <mohitmb6261@gmail.com>
2025-12-04 17:10:24 +0530

    remove_duplicate

--- lec_13/remove_duplicate.java
+++ lec_13/remove_duplicate.java
@@ -1,6 +1,32 @@
 import java.util.Scanner;
 public class remove_duplicate {
  public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+   int i,size,j;
+   System.out. println("enter a size of array");
+   size=sc.nextInt();
+    int arr[]=new int [size];
+    for(i=0;i<size;i++){
+        System.out.println("enter a element of array");
+        arr[i]=sc.nextInt();
+    }
+    System.out.println("array after removing  duplicate element = ");
+    for(i=0;i<size;i++){
+        boolean isduplicate=false;
+        for(j=0;j<i;j++){
+        if( arr[i]==arr[j]){
+            isduplicate=true;
+            break;
+        }
+    }
+    if(!isduplicate){
+        System.out.println(arr[i]);
+    }
+        
+    }
+
+    sc.close();
     
+
  }   
 }
