8f43097410c239f0f1afe1a0a89473a8d5bc6607
mohitbaghel7067 <mohitmb6261@gmail.com>
2025-12-09 21:50:28 +0530

    Iterater

--- lec_15/Iterater.java
+++ lec_15/Iterater.java
@@ -16,4 +16,4 @@ public class Iterater {
         System.out.println(List);
     }
     
-}//push
+}
---
+++ lec_15/iteraterfrommid.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class iteraterfrommid {
+    public static void main(String[] args) {
+        ArrayList<Integer>List=new ArrayList<>();
+        List.add(5);
+        List.add(6);
+        List.add(7);
+        List.add(8);
+        List.add(9);
+        List.add(10);
+        List.add(11);
+        List.add(12);
+        ListIterator<Integer>it=List.listIterator();
+        int l=List.size();
+        int m=l/2;
+        while( it.nextIndex()<=m){
+           // Integer x=it.next();
+           // System.out.println(x);
+        }
+        
+        while(it.hasPrevious()){
+            Integer y=it.previous();
+            System.out.println(y);
+        }
+    
+}
+}
