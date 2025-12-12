81799cf5f8b05dd4a2a32be4ad0360f7ae7fdbd5
mohitbaghel7067 <mohitmb6261@gmail.com>
2025-12-12 23:19:00 +0530

    marks

---
+++ lec_19/marks.java
@@ -0,0 +1,24 @@
+//package lec_19;
+
+public class marks {
+    public static void main(String[] args) {
+        int arr[]=new int [6];
+        arr[0]=35;
+        arr[1]=37;
+        arr[2]=65;
+        arr[3]=45;
+        arr[4]=98;
+        arr[5]=45;
+        for(int i=0;i<arr.length;i++){
+            System.out.println("marks  ="+arr[i]);
+           
+           
+        }
+        
+            
+        
+
+
+    }
+    
+}
