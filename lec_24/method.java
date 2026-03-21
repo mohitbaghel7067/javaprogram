456f620d5c8239ab33d3bda6b1af6c90bbbd2710
mohitbaghel7067 <mohitmb6261@gmail.com>
2026-03-21 13:57:12 +0530

    method

---
+++ lec_24/method.java
@@ -0,0 +1,11 @@
+//package lec_24;
+
+public class method {
+  public static void main(String[] args) {
+    //math wale
+    System.out.println(Math.cbrt(100));
+    double d =4.641*4.641*4.641;
+    System.out.println(d);
+
+  }  
+}
