59d4169967028f98ea0f75d6a861d3ec58eb07a2
mohitbaghel7067 <mohitmb6261@gmail.com>
2026-04-04 10:00:00 +0530

    added code on 4 April

---
+++ lec_24/secondmax.java
@@ -0,0 +1,22 @@
+public class secondmax {
+    public static void main(String[] args) {
+        int arr[]={1,2,4,6,8,10};
+        int max=arr[0];
+        int smax=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>smax && arr[i]!=max ){
+                smax=arr[i];
+            }
+        }
+        System.out.println(smax);
+        
+    
+    
+
+    }
+}
