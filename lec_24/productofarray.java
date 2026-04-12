2c1153c4327f403cee56e76d924b8c57e86bf3a2
mohitbaghel7067 <mohitmb6261@gmail.com>
2026-04-12 11:37:33 +0530

    product of array

---
+++ lec_24/productofarray.java
@@ -0,0 +1,11 @@
+public class productofarray {
+   public static void main(String[] args) {
+    int arr[]={1,4,3,5,6};
+    int product=1;
+    for(int i=0;i<arr.length;i++){
+         product= product*arr[i];
+     
+    }
+    System.out.println(product);
+   } 
+}
