68c047c35b428de87d497e1ed3497f4b4f2d5d61
mohitbaghel7067 <mohitmb6261@gmail.com>
2026-04-19 07:24:03 +0530

    printnumber

---
+++ lec_25/print5to1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class print5to1 {
+    public static void printnumber(int n){
+        if(n==0){
+            return ;
+        }
+        System.out.println(n);
+        printnumber(n-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        printnumber(n);
+    }
+}
