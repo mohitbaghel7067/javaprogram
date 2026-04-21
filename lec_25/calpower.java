7e017ba9960855c12695db2a2647f2e29e705bc2
mohitbaghel7067 <mohitmb6261@gmail.com>
2026-04-21 22:52:32 +0530

    stringrev

---
+++ lec_25/calpower.java
@@ -0,0 +1,20 @@
+public class calpower {
+    public static int calcpower(int x,int n) {
+        if(n==0){
+            return 1;
+
+        }
+        if(x==0){
+            return 0;
+        }
+        int xpower1 = calcpower(x,n-1);
+        int xpower = x * xpower1;
+        return xpower;
+    }
+    public static void main(String[] args) {
+        int x= 2,n=5;
+        int ans = calcpower( x,  n);
+        System.out.println(ans);
+    }
+    //push
+}
---
+++ lec_25/reversestring.java
@@ -0,0 +1,17 @@
+public class reversestring {
+    public static void printrev(String str,int idx) {
+        if(idx==0){
+            System.out.println(str.charAt(idx));
+            return;
+
+        }
+        System.out.print(str.charAt(idx));
+        printrev(str, idx-1);
+
+        
+    }
+    public static void main(String[] args) {
+        String str="abcd";
+        printrev(str,str.length()-1);
+    }
+}
