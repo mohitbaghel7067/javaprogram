b9473264bb79cbb847822c5b2d48f06de7fed8f3
mohitbaghel7067 <mohitmb6261@gmail.com>
2026-04-22 13:37:16 +0530

    fibo

---
+++ lec_25/fiboidx.java
@@ -0,0 +1,10 @@
+public class fiboidx {
+    public static int fibo(int n) {
+        if(n<=1) return n;
+        return fibo(n-1)+fibo(n-2);
+    }
+    public static void main(String[] args) {
+        int n=6;
+        System.out.println(fibo(n));
+    }
+}
